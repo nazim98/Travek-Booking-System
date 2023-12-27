a4092e0e6a3ba0a902fb5f4f84042bdcc310d1c1
nazim98 <t.nazim1998@gmail.com>
2023-12-27 13:08:35 +0800

    First commit

---
+++ resources/views/admin/showproduct.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    @include('admin.css')
+
+  </head>
+  <body>
+
+      <!-- partial -->
+
+      @include('admin.sidebar')
+
+      @include('admin.navbar')
+
+      <!-- partial -->
+
+      <div style="padding-bottom: 30px;" class="container-fluid page-body-wrapper">
+
+      <div class="container" style="text-align: center;">
+
+        <tr style="background-color: grey;">
+
+            <td style="padding:20px;">Title</td>
+            <td style="padding:20px;">Description</td>
+            <td style="padding:20px;">Quantity</td>
+            <td style="padding:20px;">Price</td>
+            <td style="padding:20px;">Image</td>
+            <td style="padding:20px;">Update</td>
+            <td style="padding:20px;">Delete</td>
+
+        </tr>
+
+        @foreach($data as $product)
+
+        <tr text-align="center" style="background-color: black;">
+
+            <td>{{$product->title}}</td>
+            <td>{{$product->description}}</td>
+            <td>{{$product->quantity}}</td>
+            <td>{{$product->price}}</td>
+            <td>
+                <img height="200" width="200" src="/productimage/{{$product->image}}">
+
+            </td>
+
+            <td>
+                <a class="btn btn-primary" href="">Update</a>
+            </td>
+
+            <td>
+                <a class="btn btn-danger" href="{{url('deleteproduct',$product->id)}}">Delete</a>
+            </td>
+
+        </tr>
+
+        @endforeach
+
+      </div>
+
+      </div>
+
+      <!-- partial -->
+      @include('admin.script')
+  </body>
+</html>
