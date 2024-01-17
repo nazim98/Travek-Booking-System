6d532d5c8d8d784af98f974aae37f80248e23209
nazim <t.nazim1998@gmail.com>
2024-01-17 23:14:17 +0800

    update4

--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
-
+    
     <title>Sixteen Clothing HTML Template</title>
 
     <!-- Bootstrap core CSS -->
